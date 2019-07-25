732892c220a106311f00eb6a6002564f5208744a
zhangyiwei_dxm <zhangyiwei_dxm@duxiaoman.com>
2019-07-25 10:41:23 +0800

    add quick start webflux

---
+++ springboot-webflux-1-quick/src/main/java/springboot/springbootwebflux1quick/SpringbootWebflux1QuickApplication.java
@@ -0,0 +1,13 @@
+package springboot.springbootwebflux1quick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebflux1QuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebflux1QuickApplication.class, args);
+    }
+
+}
